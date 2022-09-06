f677482f3115eb09c823464c1ee1f82610a070a2
Juan Pablo <pablo2003.seg@gmail.com>
2022-09-06 13:19:35 -0500

    se agrego el scaffold room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_160304) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_181400) do
   create_table "drivers", force: :cascade do |t|
     t.string "nombre"
     t.string "grupo"
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_160304) do
     t.string "equipo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "drivers_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["drivers_id"], name: "index_rooms_on_drivers_id"
+    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_160304) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rooms", "drivers", column: "drivers_id"
+  add_foreign_key "rooms", "users"
 end
